287518ef8d71787e01cc6f756e5d00054e6e7309
arauld <assamoiarauld@gmail.com>
2022-10-06 10:42:09 +0200

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,22 +5,23 @@ function mapStateToProps(state) {
   return {
     countValue: state.Counter,
   };
-  //Action
-  let increaseAction = { type: 'increase' };
-  let decreaseAcction = { type: 'decrease' };
-
-  //Map Redux actions to component props
-  function mapDispatchToProps(dispatch) {
-    return {
-      increaseCount: function () {
-        return dispatch(increaseAction);
-      },
-      decreaseCount: function () {
-        return dispatch(decreaseCount);
-      },
-    };
-  }
 }
+//Action
+let increaseAction = { type: 'increase' };
+let decreaseAction = { type: 'decrease' };
+
+//Map Redux actions to component props
+function mapDispatchToProps(dispatch) {
+  return {
+    increaseCount: function () {
+      return dispatch(increaseAction);
+    },
+    decreaseCount: function () {
+      return dispatch(decreaseAction);
+    },
+  };
+}
+
 //The HOC
 let connectedComponent = connect(mapStateToProps, mapDispatchToProps)(Counter);
 export default connectedComponent;
